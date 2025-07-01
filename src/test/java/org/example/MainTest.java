dde850d59ea3bc843d2d160f6ecfec3a9f6f2f1d
Torsten Bingenheimer <mjoelniradmin@gmail.com>
2025-07-01 09:30:14 +0200

    Fehler einbauen

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,77 +1,67 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PlayerCharacterTest {
-    private PlayerCharacter player;
-
-    @BeforeEach
-    void setUp() {
-        player = new PlayerCharacter();
-        player.setX(0);
-        player.setY(0);
-    }
+public class MainTest {
 
 
-    @Test
-    void getX_shouldReturn0(){
-        assertEquals(0, player.getX());
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 3",
+            "5, 5, 10"
+    })
+    public void add(int a, int b, int expected) {
+        int actual = Main.add(a, b);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void getY_shouldReturn0(){
-        assertEquals(0, player.getY());
+    void isEven_shouldReturnTrue_whenCalledWith2 (){
+        // GIVEN -> Mock data
+        int number = 2;
+        boolean expect = true;
+
+        //  WHEN -> Method cal
+        boolean actual = Main.isEven(number);
+
+        // THEN -> Assertions
+        assertEquals(expect,actual);
     }
 
     @Test
-    void moveW_shouldReturn01(){
-        int[] expected = {1,0};
-        assertArrayEquals(expected, player.moveW());
+    void multiply_shouldReturnTheCorrectNumber (){
+        assertEquals(50, Main.multiply(2,25));
+        assertEquals(50, Main.multiply(10,5));
+        assertEquals(9, Main.multiply(3,3));
     }
 
     @Test
-    void moveS_shouldReturn0minus1(){
-        int[] expected = {-1,0};
-        assertArrayEquals(expected, player.moveS());
+    void stringToUppercase_shouldReturnAnUppercaseString(){
+        assertEquals("TIM", Main.stringToUppercase("Tim"));
     }
 
     @Test
-    void moveA_shouldReturnMinus10(){
-        int[] expected = {0,-1};
-        assertArrayEquals(expected, player.moveA());
+    void isPositive_shouldReturnTrue_whenCalledWith1 (){
+        assertTrue(Main.isPositive(1));
     }
 
     @Test
-    void moveD_shouldReturn1and0(){
-        int[] expected = {0,1};
-        assertArrayEquals(expected, player.moveD());
+    void isPositive_shouldReturnFalse_whenCalledWith2 (){
+        assertFalse(Main.isPositive(0));
     }
 
     @Test
-    void moveTest_shouldReturn1and0(){
-        int[] expected = {0,1};
-        assertArrayEquals(expected, player.moveD());
+    void fizzBuzz_tdd (){
+        assertEquals("FizzBuzz", Main.fizzBuzz(15));
+        assertEquals("Fizz", Main.fizzBuzz(9));
+        assertEquals("Buzz", Main.fizzBuzz(10));
+        assertEquals("2", Main.fizzBuzz(2));
+        assertEquals("Fizz", Main.fizzBuzz(-3));
     }
 
-    @Test
-    void moveAround_shouldGoUpAndRight(){
-        int[] expected = {1,1};
-        player.moveW();
-        player.moveD();
-        assertArrayEquals(expected, player.getPosition());
-    }
-
-    @Test
-    void moveAround_shouldGoLeftLeftDownAndRight(){
-        int[] expected = {-1,-1};
-        player.moveA();
-        player.moveA();
-        player.moveS();
-        player.moveD();
-        assertArrayEquals(expected, player.getPosition());
-    }
 
 }
---
+++ src/test/java/org/example/PlayerCharacter.java
@@ -0,0 +1,39 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    }
+
+    public static int add(int a, int b) {
+        return a + a;
+    }
+
+    public static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+
+    public static int multiply(int a , int b){
+        return a * b;
+    }
+
+    public  static String stringToUppercase(String text){
+        return text.toUpperCase();
+    }
+
+    public static boolean isPositive(int num){
+        return num > 0;
+    }
+
+    public static String fizzBuzz(int num){
+        if(num % 3 == 0 && num % 5 == 0)return "FizzBuzz";
+        if(num % 5 == 0) return "Buzz";
+        if(num % 3 == 0) return "Fizz";
+        return "" + num;
+    }
+
+}
